3f0e32ec7576c439147ab9cdb0d46139bad88ee2
vhinrich <richelle.rivamonte@gmail.com>
2012-11-08 09:19:53 -0700

    first commit

--- sites/all/themes/adaptivetheme/at_vhinandrich2/templates/page.tpl.php
+++ sites/all/themes/adaptivetheme/at_vhinandrich2/templates/page.tpl.php
@@ -1,10 +1,46 @@
 <script type="text/javascript" language="javascript">
 	var hpr_timer = null;
-	var hpr_interval = 10000;
+	var hpr_interval = 8000;
 	clearInterval(hpr_timer);
 	hpr_timer = setInterval('hpr_slide()', hpr_interval);
 	
+	var max_width = 1080;
+	
 	jQuery(document).ready(function(e) {
+		
+		
+		var max_logo_mar_bot = jQuery('#logo a img').css('margin-bottom');
+		
+		if(jQuery(window).width() < max_width){
+			jQuery('.vhinrich-mp-rotator-active').css('display','none');
+			jQuery('.vhinrich-mp-rotator').css('display','block');
+		}else{
+			jQuery('.vhinrich-mp-rotator-active').css('display','block');
+			jQuery('.vhinrich-mp-rotator').css('display','none');
+		}
+		
+		jQuery(window).scroll(function(e) {
+			//alert(jQuery(this).scrollTop());
+			if(jQuery(window).width() >= max_width){
+				var mar_bot = parseInt(max_logo_mar_bot) - jQuery(this).scrollTop();
+				if(mar_bot >= jQuery('#logo a img').height() * -1){
+					jQuery('#logo a img').css('margin-bottom', mar_bot);
+				}
+			}else{
+				jQuery('#logo a img').css('margin-bottom', max_logo_mar_bot);	
+			}
+        });
+		
+		jQuery(window).resize(function(){
+			if(jQuery(window).width() < max_width){
+				jQuery('.vhinrich-mp-rotator-active').css('display','none');
+				jQuery('.vhinrich-mp-rotator').css('display','block');
+			}else{
+				jQuery('.vhinrich-mp-rotator-active').css('display','block');
+				jQuery('.vhinrich-mp-rotator').css('display','none');
+			}
+		});
+		
 		if(jQuery('.pager-load-more').length == 0){
 			jQuery('#back-to-top-wrapper').css('display','none');		
 		}else{
@@ -38,45 +74,141 @@
 			}
         });
 		
+		jQuery('#primary-menu-bar ul li.expanded').each(function(index, element) {
+            jQuery(this).append('<div class="primary-menu-bar-submenu"></div>');
+			jQuery(this).children('.primary-menu-bar-submenu').html(jQuery(element).children('ul'));
+        });
+		
+		var timer = null;
+		var curr_menu = null;
 		jQuery('#primary-menu-bar ul li.expanded').hover(
 			function(e){
-				jQuery(this).children('ul.menu').css('display','inline');
+				if(timer){
+					clearTimeout(timer);
+					timer = null;
+				}
+				curr_menu = jQuery(this);
+				timer = setTimeout(function(){
+						
+					//jQuery(this).children('ul.menu').css('display','inline');
+					jQuery('.primary-menu-bar-submenu ul li').css('opacity','0');
+					jQuery('.primary-menu-bar-submenu ul li').css('display','inline');
+					jQuery('.primary-menu-bar-submenu ul li').css('visibility','visible');
+					jQuery('.primary-menu-bar-submenu ul').animate(
+						{
+							height:40
+						},200,function(){
+							jQuery('.primary-menu-bar-submenu ul li').animate(
+								{
+									opacity:1
+								},200,function(){
+									jQuery('.primary-menu-bar-submenu ul li').css('display','inline-block');
+									jQuery('.primary-menu-bar-submenu ul li').css('visibility','visible');
+									jQuery('.primary-menu-bar-submenu ul li').css('opacity','1');
+								}
+							);
+						}
+					)
+				},100);
 			},
 			function(e){
-				jQuery(this).children('ul.menu').css('display','none');
+				//jQuery(this).children('ul.menu').css('display','none');
+				if(timer){
+					clearTimeout(timer);
+					timer = null;
+				}
+				jQuery('.primary-menu-bar-submenu ul li').css('display','none');
+				jQuery('.primary-menu-bar-submenu ul li').css('visibility','hidden');
+				jQuery('.primary-menu-bar-submenu ul li').css('opacity','0');
+				jQuery('.primary-menu-bar-submenu ul').animate(
+					{height:0},100,function(){
+						jQuery('.primary-menu-bar-submenu ul li').animate(
+							{
+								opacity:0
+							},100,function(){
+								jQuery('.primary-menu-bar-submenu ul li').css('display','none');
+								jQuery('.primary-menu-bar-submenu ul li').css('visibility','hiddedn');
+								jQuery('.primary-menu-bar-submenu ul li').css('opacity','0');
+							}
+						);
+					}
+				);
 			}
 		);
     });
 	
 	hpr_slide = function(){
-		clearInterval(hpr_timer);
-		var hpr_active_id = (jQuery('.hpr-item.active').attr('id'));
-		if(hpr_active_id){
-			hpr_active_id = hpr_active_id.replace('hpr-item-','');
-		}
-		
-		jQuery('#hpr-item-' + hpr_active_id).animate(
-			{
-				opacity:0
-			},500,function(){
-				var hpr_item_count = jQuery('.hpr-item').length;
-				if(hpr_item_count == hpr_active_id){
-					hpr_active_id = 1;
-				}else{
-					hpr_active_id = parseInt(hpr_active_id) + parseInt(1);
-				}
-				jQuery('.hpr-item').removeClass('active');
-				jQuery('#hpr-item-' + hpr_active_id).addClass('active');
-				jQuery('#hpr-item-' + hpr_active_id).css('opacity',0);
-				jQuery('#hpr-item-' + hpr_active_id).animate(
-					{
-						opacity:1
-					},500,function(){
-						hpr_timer = setInterval('hpr_slide()', hpr_interval);
+		if(jQuery(window).width() >= max_width){
+			clearInterval(hpr_timer);
+			jQuery('.vhinrich-mp-rotator-active .hpr-item.active').animate(
+				{
+					opacity:0
+				},1000,function(){
+					var hpr_active_id = (jQuery('.vhinrich-mp-rotator .hpr-item.active').attr('id'));
+					if(hpr_active_id){
+						hpr_active_id = hpr_active_id.replace('hpr-item-','');
 					}
-				);
+					var hpr_item_count = jQuery('.vhinrich-mp-rotator .hpr-item').length;
+					if(hpr_item_count == hpr_active_id){
+						hpr_active_id = 1;
+					}else{
+						hpr_active_id = parseInt(hpr_active_id) + parseInt(1);
+					}
+					jQuery('.vhinrich-mp-rotator .hpr-item').removeClass('active');
+					jQuery('.vhinrich-mp-rotator #hpr-item-' + hpr_active_id).addClass('active');
+					
+					jQuery('.vhinrich-mp-rotator-active').html('');
+					
+					var hpr_next_active_id = hpr_active_id;
+					if(hpr_active_id == hpr_item_count){
+						hpr_next_active_id = 1;
+					}else{
+						hpr_next_active_id = parseInt(hpr_active_id) + parseInt(1);
+					}
+					
+					var active_li = jQuery('.vhinrich-mp-rotator #hpr-item-' + hpr_active_id).html();
+					active_li = '<li class="hpr-item active" id="hpr-item-1">' + active_li + '</li>';
+					var next_li = jQuery('.vhinrich-mp-rotator #hpr-item-' + hpr_next_active_id).html();
+					next_li = '<li class="hpr-item" id="hpr-item-2">' + next_li + '</li>';
+					
+					jQuery('.vhinrich-mp-rotator-active').append(active_li);
+					jQuery('.vhinrich-mp-rotator-active').append(next_li);
+					
+					jQuery('.vhinrich-mp-rotator-active .hpr-item').css('z-index',0);
+					jQuery('.vhinrich-mp-rotator-active .hpr-item.active').css('z-index',1);
+					
+					
+					hpr_timer = setInterval('hpr_slide()', hpr_interval);
+				}
+			);
+			/*var hpr_active_id = (jQuery('.hpr-item.active').attr('id'));
+			if(hpr_active_id){
+				hpr_active_id = hpr_active_id.replace('hpr-item-','');
 			}
-		);
+			
+			jQuery('#hpr-item-' + hpr_active_id).animate(
+				{
+					opacity:0
+				},500,function(){
+					var hpr_item_count = jQuery('.hpr-item').length;
+					if(hpr_item_count == hpr_active_id){
+						hpr_active_id = 1;
+					}else{
+						hpr_active_id = parseInt(hpr_active_id) + parseInt(1);
+					}
+					jQuery('.hpr-item').removeClass('active');
+					jQuery('#hpr-item-' + hpr_active_id).addClass('active');
+					jQuery('#hpr-item-' + hpr_active_id).css('opacity',0);
+					jQuery('#hpr-item-' + hpr_active_id).animate(
+						{
+							opacity:1
+						},500,function(){
+							hpr_timer = setInterval('hpr_slide()', hpr_interval);
+						}
+					);
+				}
+			);*/
+		}
 	}
 	
 	jQuery(document).ready(function(e) {
@@ -100,27 +232,37 @@
 <div id="page-top">
   <header class="clearfix vhinrich-header" role="banner">
 
-	<div class="nav" id="primary-menu-bar"><nav class="clearfix">
-    <?php 
-		$main_menu = variable_get('menu_main_links_source', 'main-menu');
-		$tree = menu_tree($main_menu);
-		print drupal_render($tree);
-	?>
-    </nav></div>
+	<div id="top-menu-bar">
+        <div class="nav" id="primary-menu-bar"><nav class="clearfix">
+        <?php 
+            $main_menu = variable_get('menu_main_links_source', 'main-menu');
+            $tree = menu_tree($main_menu);
+            print drupal_render($tree);
+        ?>
+        </nav></div>
+        <!--<div id="primary-menu-bar-submenu">
+            <nav class="clearfix">
+                <ul>
+                    <li><a href="#">ABC</a></li>
+                    <li><a href="#">DEF</a></li>
+                </ul>
+            </nav>
+        </div>-->
+    </div>
     
     <?php if ($linked_site_logo): ?>
       <!--<div id="logo"><?php print $linked_site_logo; ?></div>-->
     <?php endif; ?>
 
     <?php if ($site_name || $site_slogan): ?>
-      <!--<hgroup<?php if (!$site_slogan && $hide_site_name): ?> class="<?php print $visibility; ?>"<?php endif; ?>>
+      <hgroup<?php if (!$site_slogan && $hide_site_name): ?> class="<?php print $visibility; ?>"<?php endif; ?> style="display:none">
         <?php if ($site_name): ?>
           <h1 id="site-name"<?php if ($hide_site_name): ?> class="<?php print $visibility; ?>"<?php endif; ?>><?php print $site_name; ?></h1>
         <?php endif; ?>
         <?php if ($site_slogan): ?>
           <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
-      </hgroup>-->
+      </hgroup>
     <?php endif; ?>
 
     <?php print render($page['header']); ?>
@@ -188,7 +330,27 @@
 			$homepage_rotator_view = views_get_view_result('home_page_rotator', 'default');
 		?>
 		<div id="vhinrich-mp-rotator">
-			<ul class="vhinrich-mp-rotator">
+        	<ul class="vhinrich-mp-rotator-active">
+            	<?php $i = 0; ?>
+				<?php foreach($homepage_rotator_view as $homepage_rotator): ?>
+                	<?php 
+						$hpr_node = node_load($homepage_rotator->nid);
+						$main_image_node = node_load($hpr_node->field_main_image['und'][0]['nid']); 
+						$img_arr = array(
+							'item' => $main_image_node->field_image['und'][0],
+							'image_style' => 'home-page-rotator-2'
+						);
+						$image_rotator = theme_image_formatter($img_arr);
+						$i++;
+					?>
+                    <?php if($i<=2): ?>
+                        <li id="hpr-item-<?php print $i; ?>" class="hpr-item <?php if($i == 1){ print 'active';} ?>" style="z-index:<?php print 2-$i; ?>">
+                            <?php print $image_rotator; ?>
+                        </li>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </ul>
+			<ul class="vhinrich-mp-rotator"  style="display:none">
 				<?php $i = 0; ?>
 				<?php foreach($homepage_rotator_view as $homepage_rotator): ?>
 					<?php 
