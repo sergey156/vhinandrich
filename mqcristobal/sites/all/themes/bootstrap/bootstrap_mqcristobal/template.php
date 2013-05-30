85cb33c509b0647aac2028dcf004e0e9a4a203ca
vhin0210 <vhin0210@gmail.com>
2013-05-30 23:00:42 +0800

    fix jquery in admin; theme parallax; theme instagram block

--- mqcristobal/sites/all/themes/bootstrap/bootstrap_mqcristobal/template.php
+++ mqcristobal/sites/all/themes/bootstrap/bootstrap_mqcristobal/template.php
@@ -8,3 +8,17 @@ function bootstrap_mqcristobal_preprocess_node(&$vars) {
 	$vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->type . '__' . strtolower($vars['view_mode']);   
 	$vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->nid . '__' . strtolower($vars['view_mode']);
 }
+
+
+
+
+function _bsmqc_get_image_path($node, $image_style){
+	$url = array();
+	$img = $node->field_image;
+	if(isset($img[LANGUAGE_NONE])){
+		foreach($img[LANGUAGE_NONE] as $image){
+			$url[] = image_style_url($image_style, $image['uri']);
+		}
+	}
+	return $url;
+}
--- mqcristobal/sites/all/themes/bootstrap/bootstrap_mqcristobal/templates/node--page--parallax.tpl.php
+++ mqcristobal/sites/all/themes/bootstrap/bootstrap_mqcristobal/templates/node--page--parallax.tpl.php
@@ -13,15 +13,36 @@
       </span>
     <?php endif; ?>
   </header>
-
+  
   <?php
-    // Hide comments, tags, and links now so that we can render them later.
-    hide($content['comments']);
-    hide($content['links']);
-    hide($content['field_tags']);
-    print render($content);
+    $bg_color = '';
+    if($tmp_bg_color = reset($field_bg_color)){
+      if(isset($tmp_bg_color['rgb']))
+        $bg_color = $tmp_bg_color['rgb'];
+    }
+    
+    $bg_image = '';
+    if($tmp_bg_image_node = reset($field_node_bg_image)){
+      if(isset($tmp_bg_image_node['node'])){
+        $tmp_bg_image = _bsmqc_get_image_path($tmp_bg_image_node['node'], 'page_parallax_bg_image');
+        foreach($tmp_bg_image as $img){
+          $bg_image = $img;
+        }
+      }
+    }
+    if($bg_image)
+      $bg_image = "url('" . $bg_image . "')";
   ?>
-
+  <div style="background: <?php print $bg_image; ?> <?php print $bg_color; ?>">
+    <?php
+      // Hide comments, tags, and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      hide($content['field_tags']);
+      print render($content);
+    ?>
+  </div>
+  
   <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
     <footer>
       <?php print render($content['field_tags']); ?>
