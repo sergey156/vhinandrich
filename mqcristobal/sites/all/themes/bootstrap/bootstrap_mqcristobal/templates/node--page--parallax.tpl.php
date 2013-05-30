f73a71a9d153d2851505656b8307e0c96de7006b
vhin0210 <vhin0210@gmail.com>
2013-05-30 23:22:52 +0800

    debug parallax template

--- mqcristobal/sites/all/themes/bootstrap/bootstrap_mqcristobal/templates/node--page--parallax.tpl.php
+++ mqcristobal/sites/all/themes/bootstrap/bootstrap_mqcristobal/templates/node--page--parallax.tpl.php
@@ -1,3 +1,22 @@
+<?php
+  $bg_color = '';
+  if($tmp_bg_color = reset($field_bg_color)){
+    if(isset($tmp_bg_color['rgb']))
+      $bg_color = $tmp_bg_color['rgb'];
+  }
+  
+  $bg_image = '';
+  if($tmp_bg_image_node = reset($field_node_bg_image)){
+    if(isset($tmp_bg_image_node['node'])){
+      $tmp_bg_image = _bsmqc_get_image_path($tmp_bg_image_node['node'], 'page_parallax_bg_image');
+      foreach($tmp_bg_image as $img){
+        $bg_image = $img;
+      }
+    }
+  }
+  if($bg_image)
+    $bg_image = "url('" . $bg_image . "')";
+?>
 <div style="background: <?php print $bg_image; ?> <?php print $bg_color; ?>">
   <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
     <header>
@@ -14,26 +33,6 @@
         </span>
       <?php endif; ?>
     </header>
-    
-    <?php
-      $bg_color = '';
-      if($tmp_bg_color = reset($field_bg_color)){
-        if(isset($tmp_bg_color['rgb']))
-          $bg_color = $tmp_bg_color['rgb'];
-      }
-      
-      $bg_image = '';
-      if($tmp_bg_image_node = reset($field_node_bg_image)){
-        if(isset($tmp_bg_image_node['node'])){
-          $tmp_bg_image = _bsmqc_get_image_path($tmp_bg_image_node['node'], 'page_parallax_bg_image');
-          foreach($tmp_bg_image as $img){
-            $bg_image = $img;
-          }
-        }
-      }
-      if($bg_image)
-        $bg_image = "url('" . $bg_image . "')";
-    ?>
     <?php
       // Hide comments, tags, and links now so that we can render them later.
       hide($content['comments']);
